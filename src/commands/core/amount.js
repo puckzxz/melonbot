a09a87cb42c749f5d2bf30f1a95c1cd641e1e03c
Chris <cbock1337@gmail.com>
2018-11-13 00:57:17 -0500

    fixed some commands

--- src/commands/core/amount.js
+++ src/commands/core/amount.js
@@ -1,57 +1,57 @@
 const {
-  Command
+    Command
 } = require('discord.js-commando');
 const chalk = require('chalk');
 const path = require('path');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config({
-  path: path.join(__dirname, '.env')
+    path: path.join(__dirname, '.env')
 });
 let mongoURL = process.env.MONGO_URL;
 
 module.exports = class SubmitCommand extends Command {
-  constructor(client) {
-    super(client, {
-      name: 'amount',
-      group: 'core',
-      memberName: 'amount',
-      description: 'Gets the amount of times a movie is in the Melon Patch Movie Database!',
-      examples: ['amount <MOVIE NAME>'],
-      guildOnly: true,
-      args: [{
-        key: 'title',
-        prompt: 'What tite would you like to search?',
-        type: 'string',
-      }]
-    });
-  }
+    constructor(client) {
+        super(client, {
+            name: 'amount',
+            group: 'core',
+            memberName: 'amount',
+            description: 'Gets the amount of times a movie is in the Melon Patch Movie Database!',
+            examples: ['amount <MOVIE NAME>'],
+            guildOnly: true,
+            args: [{
+                key: 'title',
+                prompt: 'What title would you like to search?',
+                type: 'string',
+            }]
+        });
+    }
 
-  async run(msg, args) {
-    var message = await msg.say(`Getting the amount of entries for **${args.title}**...`);
-    getAmountByTitle(args.title, function (result) {
-      return message.edit(`**${args.title}** has **${result.length}** entires in the database!`);
-    });
-  }
+    async run(msg, args) {
+        var message = await msg.say(`Getting the amount of entries for **${args.title}**...`);
+        getAmountByTitle(args.title, function (result) {
+            return message.edit(`**${args.title}** has **${result.length}** entires in the database!`);
+        });
+    }
 };
 
 function getAmountByTitle(movieTitle, callback) {
-  MongoClient.connect(mongoURL, {
-    useNewUrlParser: true
-  }, function (err, db) {
-    if (err) {
-      console.log(chalk.red(err));
-    }
-    var dbo = db.db(process.env.MONGO_DB);
-    dbo.collection(process.env.MONGO_COL).find({
-      'Title': {
-        $in: [`${movieTitle}`]
-      }
-    }).toArray(function (err, result) {
-      if (err) {
-        console.log(chalk.red(err));
-      }
-      db.close();
-      callback(result);
+    MongoClient.connect(mongoURL, {
+        useNewUrlParser: true
+    }, function (err, db) {
+        if (err) {
+            console.log(chalk.red(err));
+        }
+        var dbo = db.db(process.env.MONGO_DB);
+        dbo.collection(process.env.MONGO_COL).find({
+            'Title': {
+                $in: [`${movieTitle}`]
+            }
+        }).toArray(function (err, result) {
+            if (err) {
+                console.log(chalk.red(err));
+            }
+            db.close();
+            callback(result);
+        });
     });
-  });
 }
--- src/commands/core/delete.js
+++ src/commands/core/delete.js
@@ -1,54 +1,54 @@
 const {
-  Command
+    Command
 } = require('discord.js-commando');
 const chalk = require('chalk');
 const path = require('path');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config({
-  path: path.join(__dirname, '.env')
+    path: path.join(__dirname, '.env')
 });
 let mongoURL = process.env.MONGO_URL;
 
 module.exports = class SubmitCommand extends Command {
-  constructor(client) {
-    super(client, {
-      name: 'delete',
-      group: 'core',
-      memberName: 'delete',
-      description: 'Deletes a movie from the Melon Patch Movie Database!',
-      examples: ['delete <MOVIE NAME>'],
-      guildOnly: true,
-      args: [{
-        key: 'title',
-        prompt: 'What tite would you like to search?',
-        type: 'string',
-      }]
-    });
-  }
+    constructor(client) {
+        super(client, {
+            name: 'delete',
+            group: 'core',
+            memberName: 'delete',
+            description: 'Deletes a movie from the Melon Patch Movie Database!',
+            examples: ['delete <MOVIE NAME>'],
+            guildOnly: true,
+            args: [{
+                key: 'title',
+                prompt: 'What tite would you like to search?',
+                type: 'string',
+            }]
+        });
+    }
 
-  async run(msg, args) {
-    var message = await msg.say("Deleting movie from database...");
-    deleteMovieByTitle(args.title);
-    message.edit(`Deleted **${args.title}** from the database`);
-  }
+    async run(msg, args) {
+        var message = await msg.say(`Deleting ${args.title} from the database...`);
+        deleteMovieByTitle(args.title);
+        message.edit(`Deleted **${args.title}** from the database`);
+    }
 };
 
 function deleteMovieByTitle(movieTitle) {
-  MongoClient.connect(mongoURL, {
-    useNewUrlParser: true
-  }, function (err, db) {
-    if (err) {
-      console.log(chalk.red(err));
-    };
-    var dbo = db.db(process.env.MONGO_DB);
-    dbo.collection(process.env.MONGO_COL).deleteOne({
-      'Title': `${movieTitle}`
-    }, function (err, obj) {
-      if (err) {
-        console.log(chalk.red(err));
-      }
-      console.log(`Deleted ${movieTitle}`);
-      db.close();
+    MongoClient.connect(mongoURL, {
+        useNewUrlParser: true
+    }, function (err, db) {
+        if (err) {
+            console.log(chalk.red(err));
+        };
+        var dbo = db.db(process.env.MONGO_DB);
+        dbo.collection(process.env.MONGO_COL).deleteOne({
+            'Title': `${movieTitle}`
+        }, function (err, obj) {
+            if (err) {
+                console.log(chalk.red(err));
+            }
+            console.log(`Deleted ${movieTitle}`);
+            db.close();
+        });
     });
-  });
 }
