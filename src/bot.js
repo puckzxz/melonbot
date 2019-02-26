b0230960faed392fd3c55cf361c71be047450df2
Chris <cbock1337@gmail.com>
2019-02-25 22:40:26 -0500

    dynamic status

--- src/bot.js
+++ src/bot.js
@@ -5,6 +5,19 @@ require("dotenv").config({
   path: path.join(__dirname, ".env")
 });
 
+const statuses = [
+  "with Puck",
+  "with Ethik",
+  "with Virus",
+  "with Death",
+  "with Infms",
+  "with Fakes",
+  "with HoodieThief",
+  "with Sepa",
+  "with Jefe",
+  "with melons"
+];
+
 const client = new Client({
   owner: ["135554522616561664", "190220855609917442"],
   commandPrefix: "$",
@@ -27,11 +40,12 @@ client
         `Client logged in\nLogged in as ${client.user.username}#${
           client.user.discriminator
         } - ${client.user.id}`
-      )
+      ),
+      setInterval(() => {
+        const index = Math.floor(Math.random() * (statuses.length - 1) + 1);
+        client.user.setActivity(statuses[index]);
+      }, 30000) // run every 30 seconds
     );
-    client.user.setActivity("with melons", {
-      type: "PLAYING"
-    });
   })
   .on("disconnect", () => {
     console.log(chalk.yellow("Client disconnected!"));
