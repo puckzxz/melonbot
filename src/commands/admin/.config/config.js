58bbc74b18e8c2513cc71d7d8eb0952ead82786d
Chris <cbock1337@gmail.com>
2019-04-22 22:07:55 -0400

    cleaned up kick message

--- src/commands/admin/.config/config.js
+++ src/commands/admin/.config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  DefaultKickMessage: "Kicked by melonbot using the $kick command, no reason specified.",
+  DefaultKickMessage: "Kicked by melonbot, no reason specified.",
   DefaultBanMessage: "Banned by melonbot, no reason specified."
 };
