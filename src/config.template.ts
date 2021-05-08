1499234ad5323d9ff2c95007765d2c42f84f63c5
Chris Bockover <cbock1337@gmail.com>
2021-05-08 17:53:37 -0400

    fix packages

--- src/config.template.ts
+++ src/config.template.ts
@@ -1,5 +1,6 @@
 export const DEBUG = false;
 export const DEFAULT_BAN_MESSAGE = "";
 export const DEFAULT_KICK_MESSAGE = "";
+export const OMDB_API_KEY = "";
 export const TOKEN = "";
 export const TOKEN_DEV = ""; // This isn't needed if you keep DEBUG to false
